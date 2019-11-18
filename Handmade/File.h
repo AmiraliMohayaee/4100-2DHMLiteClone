45f97a9543c8b7d931548a8e51e49fb294ca31f0
Amirali Mohayaee <amig90@yahoo.com>
2019-11-18 01:30:56 +0000

    File Manager Added

---
+++ Handmade/File.h
@@ -0,0 +1,27 @@
+#ifndef FILE_H
+#define FILE_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+
+class File
+{
+public:
+	File();
+	File(std::string& filename);
+	~File();
+	bool CreateAFile(std::string filename);
+	bool WriteToFile(std::string writing);
+	bool ReadFile(const std::string filename);	// perhaps an int param for line num?
+	bool CloseFile();
+	std::string GetLine(int lineNum);
+
+private:
+	std::fstream* m_openFile;
+	std::string m_fileString;
+};
+
+
+#endif
